c36f970b69819143925ab4dee3bfd7159b909ace
ma-X71 <albert.maxans@gmail.com>
2026-05-10 16:10:09 +0200

    nouveau test avec packagejson

--- api/preview.js
+++ api/preview.js
@@ -1,4 +1,3 @@
-// api/preview.js
 export default async function handler(req, res) {
   const { id } = req.query;
   if (!id) return res.status(400).json({ error: 'ID manquant' });
@@ -9,7 +8,7 @@ export default async function handler(req, res) {
 
     if (!data.preview) return res.status(404).json({ error: 'Preview introuvable' });
 
-    // Correction indispensable pour éviter le blocage navigateur
+    // IMPORTANT : Remplace http par https
     const secureUrl = data.preview.replace('http://', 'https://');
     res.status(200).json({ url: secureUrl });
   } catch (e) {
